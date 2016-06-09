4d272276228a8c32f6e63eaa9d29eec20baf6fbf
Simon Busborg <simon@umbraco.com>
2016-06-09 15:53:39 +0200

    added jquery to app.js updated templates

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,16 @@
 ;(function(){
 	'use strict';
 
-	// Start writing smart ass code here
+	$(document).ready(function(){
+		$(window).bind('scroll', function() {
+			var navHeight = 100;
+			  if ($(window).scrollTop() > navHeight) {
+				  $('body').addClass('fixed');
+			  }
+			  else {
+				  $('body').removeClass('fixed');
+			  }
+		 });
+	 });
 
 }());
